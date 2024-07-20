92945f601b0f2d467d6e960d3368ce88a36b66e9
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2024-07-20 15:51:26 +0530

    db chnged

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ from os import path, getenv
 class Config:
     API_ID = 8623612  #your API ID
     API_HASH = "06ea2889c5517eb64017b032d667e29f"  #your API HASH
-    BOT_TOKEN = "6044927937:AAHjidOZCsPLsbRXFR4oqBe-rwXYKIkTUZg"  #BOT TOKEN
-    MONGO_URI = "mongodb+srv://samantha:kBGnehdhUgN1XNWy@cluster0.witnkuy.mongodb.net/?retryWrites=true&w=majority"
-    SUDO = [1853733618, 1467358214]
-    CHID = -1001820582259
-    
+    BOT_TOKEN = "6044927937:AAFak7TFl5Ic3nwqwgeZXQfjXDMP2gC28Us"  #BOT TOKEN
+    MONGO_URI = "mongodb+srv://admin:admin@cluster0.tljjz3n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    ##MONGO_URI = "mongodb+srv://samantha:kBGnehdhUgN1XNWy@cluster0.witnkuy.mongodb.net/?retryWrites=true&w=majority"
+    SUDO = [1853733618]
+    #CHID = -1001820582259
+    CHID = -1001584640162
 cfg = Config()
